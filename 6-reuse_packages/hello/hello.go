5e8dca9a3f86f0ce035ec7c8d4b1fb45f0f0c4d5
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2022-04-03 22:49:03 +0000

    feat: randomize the hello message

--- 6-reuse_packages/hello/hello.go
+++ 6-reuse_packages/hello/hello.go
@@ -3,12 +3,28 @@ package hello
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("name is empty")
 	}
-	message := fmt.Sprintf("Hello, %v!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hello, %v!",
+		"I'm glad to see u back, %v!",
+		"Where were you? There is 3 spiderman now!! %v",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
